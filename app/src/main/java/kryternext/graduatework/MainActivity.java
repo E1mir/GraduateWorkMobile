9e84bf91c16c335f2f0ddada0b42189d875bd874
KryTerNexT <kryternext@gmail.com>
2017-09-02 00:43:50 +0400

    Created main layout.

--- app/src/main/java/kryternext/graduatework/MainActivity.java
+++ app/src/main/java/kryternext/graduatework/MainActivity.java
@@ -2,6 +2,8 @@ package kryternext.graduatework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showAbout(View view) {
+        Toast.makeText(this, "Copyright", Toast.LENGTH_SHORT).show();
+    }
+
+    public void logIn(View view) {
+        Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show();
+    }
+
+    public void register(View view) {
+        Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show();
     }
 }
