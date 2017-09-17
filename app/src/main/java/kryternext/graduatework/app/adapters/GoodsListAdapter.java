7ff4b4775690f2103cb6ffb10514cb5f35f67316
KryTerNexT <kryternext@gmail.com>
2017-09-17 18:33:13 +0400

    Simple functionality added.

--- app/src/main/java/kryternext/graduatework/app/adapters/GoodsListAdapter.java
+++ app/src/main/java/kryternext/graduatework/app/adapters/GoodsListAdapter.java
@@ -2,10 +2,12 @@ package kryternext.graduatework.app.adapters;
 
 
 import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -50,9 +52,38 @@ public class GoodsListAdapter extends BaseAdapter {
         TextView name = (TextView) productRow.findViewById(R.id.productName);
         TextView price = (TextView) productRow.findViewById(R.id.productPrice);
         TextView count = (TextView) productRow.findViewById(R.id.productCount);
+        final int availableCount = product.getCount();
+        final TextView selectedCount = (TextView) productRow.findViewById(R.id.productSelectedCount);
+        Button minus = (Button) productRow.findViewById(R.id.minusProduct);
+        Button plus = (Button) productRow.findViewById(R.id.plusProduct);
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String countStr = selectedCount.getText().toString();
+                int count = Integer.parseInt(countStr);
+                if (count > 0) {
+                    selectedCount.setText(String.valueOf(--count));
+                } else {
+                    Snackbar.make(v, "Count is 0", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            }
+        });
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String countStr = selectedCount.getText().toString();
+                int count = Integer.parseInt(countStr);
+                if (count < availableCount) {
+                    selectedCount.setText(String.valueOf(++count));
+                } else {
+                    Snackbar.make(v, "Maximal product count reached!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            }
+        });
         name.setText(String.format(Locale.ENGLISH, "%d: %s", (position + 1), product.getProductName()));
         price.setText(String.format(Locale.ENGLISH, "%.2f$", product.getPrice()));
         count.setText(String.format(Locale.ENGLISH, "%d", product.getCount()));
+        selectedCount.setText("0");
         return productRow;
     }
 }
